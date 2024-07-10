afb3298845ba81b75bed5022eb0d9684dbc8cada
CHOMINWOONG <alsdnd0701@naver.com>
2024-07-10 16:02:30 +0900

    clean push

---
+++ src/main/java/com/example/dietexercise/DietexerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.dietexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietexerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dietexercise/DietexerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dietexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietexerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
